069ffe9ad76b63497e19fd8e3968068f7d0c5730
Vivek Clement <vivekclement@gmail.com>
2023-11-11 17:32:55 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+
+    // console.log(data.slip.advice);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Free Advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have been adviced <strong>{props.count}</strong> times today.
+    </p>
   );
 }
